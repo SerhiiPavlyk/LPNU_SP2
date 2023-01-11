585d1f422dd3232e43effc479de738f18d1f8341
Serhii Pavlyk <sergijpavlik@gmail.com>
2023-01-11 18:50:16 +0200

    add LAB4
    completed laboratory work №4 for the variant 14

---
+++ SP2_LAB4/SP2_LAB4.cpp
@@ -0,0 +1,14 @@
+﻿#include "..\SP2_LAB4_DLL\dllmain.h"
+#include <string>
+int main()
+{
+    std::string text;
+    while (true)
+    {
+        std::cout << "Enter text:\n";
+        std::getline(std::cin, text);
+        std::cout << removeMiddleLetter(text) << std::endl;
+    }
+    system("pause");
+    return 0;
+}
